bf2d40dd3436effb4ca2e21878f6fb1a3f275cba
Paweł Niedźwiecki <hello@uxu.pl>
2022-06-06 15:29:19 +0200

    i update structure

--- src/components/atoms/button/component.button.index.tsx
+++
@@ -1,5 +0,0 @@
-import Button from "./button/component.button";
-import ButtonLinkIn from "./linkIn/component.buttonLinkIn";
-import ButtonSubmit from "./submit/component.buttonSubmit";
-
-export { Button, ButtonLinkIn, ButtonSubmit };
