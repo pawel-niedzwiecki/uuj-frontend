6c3758b07db88b572b9e8dcf5678af6ba6408d83
Paweł Niedźwiecki <hello@uxu.pl>
2022-06-12 21:12:04 +0200

    update srticles

--- src/pages/[...articles].tsx
+++ src/pages/[...articles].tsx
@@ -3,10 +3,10 @@ import styled from "styled-components";
 import { slugFromTitle } from "utils/utils.slug";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { displayMenu, DisplayMenuType } from "database/menu";
-import { displayCategory, DisplayCategoryType } from "database/categories";
-import { displayNewsListOnBackend, DisplayNewsListType, NewsListType, displayListNewsPageOnBackEnd, DisplayListNewsPageType, DataListNewsPageType } from "database/news";
-import { displayContactOnBackEnd, DisplayContactType } from "database/contact";
 import { ComponentSectionNewsList } from "components/templates/section";
+import { displayCategory, DisplayCategoryType } from "database/categories";
+import { displayContactOnBackEnd, DisplayContactType } from "database/contact";
+import { displayNewsListOnBackend, DisplayNewsListType, NewsListType, displayListNewsPageOnBackEnd, DisplayListNewsPageType, DataListNewsPageType } from "database/news";
 
 const Break = styled.div`
   display: block;
@@ -18,30 +18,11 @@ const Break = styled.div`
   }
 `;
 
-function Article({
-  menuHeader,
-  menuFooterUseful,
-  menuFooterForCustomers,
-  menuFooterForMedia,
-  contact,
-  dataListNewsPage,
-}: {
-  menuHeader: DisplayMenuType;
-  menuFooterUseful: DisplayMenuType;
-  menuFooterForCustomers: DisplayMenuType;
-  menuFooterForMedia: DisplayMenuType;
-  contact: DisplayContactType;
-  dataListNewsPage: DisplayListNewsPageType;
-}) {
+function Article({ menuHeader, menuFooterUseful, menuFooterForCustomers, menuFooterForMedia }: { menuHeader: DisplayMenuType; menuFooterUseful: DisplayMenuType; menuFooterForCustomers: DisplayMenuType; menuFooterForMedia: DisplayMenuType }) {
   return (
-    <Laout
-      data={{
-        header: { menu: menuHeader, contact },
-        footer: { socialMedia: contact.data?.attributes.social_media, mainAddress: contact.data?.attributes.main_address, branches: contact.data?.attributes.branches, description: dataListNewsPage.data?.attributes.description_page, menuFooterUseful, menuFooterForCustomers, menuFooterForMedia },
-      }}
-    >
+    <>
       <Break />
-    </Laout>
+    </>
   );
 }
 
@@ -50,25 +31,22 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   const allArticle: any[] = await Promise.all(
     new Array(articles.meta.pagination.pageCount).fill(undefined).map(async (_: undefined, i: number): Promise<any> => {
-      const articeWithOnlyTitle: DisplayNewsListType = await displayNewsListOnBackend({ page: i });
+      const articeWithOnlyTitle: DisplayNewsListType = await displayNewsListOnBackend({ page: i + 1 });
       return articeWithOnlyTitle?.data;
     })
   );
 
   return {
-    paths: [].concat.apply([], allArticle).map((item: NewsListType) => `/article/${item.id}/${slugFromTitle(item?.attributes.title)}`),
+    paths: [].concat.apply([], allArticle).map((item: NewsListType) => `/article/${item.id}/${slugFromTitle(item.attributes.title)}`),
     fallback: true,
   };
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  console.log(context);
   const menuHeader: DisplayMenuType = await displayMenu({ name: "header" });
   const menuFooterUseful: DisplayMenuType = await displayMenu({ name: "useful" });
   const menuFooterForCustomers: DisplayMenuType = await displayMenu({ name: "for-customers" });
   const menuFooterForMedia: DisplayMenuType = await displayMenu({ name: "for-media" });
-  const dataListNewsPage: DisplayListNewsPageType = await displayListNewsPageOnBackEnd({ seo: true });
-  const contact: DisplayContactType = await displayContactOnBackEnd({ numberPhones: true, email: true, socialMedia: true, mainAddress: true, branches: true });
 
   return {
     props: {
@@ -76,8 +54,6 @@ export const getStaticProps: GetStaticProps = async (context) => {
       menuFooterUseful,
       menuFooterForCustomers,
       menuFooterForMedia,
-      contact,
-      dataListNewsPage,
     },
   };
 };
