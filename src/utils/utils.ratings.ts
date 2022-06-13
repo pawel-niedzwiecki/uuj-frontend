80d1ad0461bcac23c9337c696525114deedcf982
Paweł Niedźwiecki <hello@uxu.pl>
2022-06-13 20:15:29 +0200

    update

--- src/utils/utils.ratings.ts
+++ src/utils/utils.ratings.ts
@@ -1,9 +1,9 @@
 import { RatingType } from "database/home";
 
-export const countRaitings = ({ ratings }: { ratings: Rating[] | undefined }) => {
+export const countRaitings = ({ ratings }: { ratings: RatingType[] | undefined }) => {
   let ratingsMean = 0;
 
-  !!ratings && ratings.forEach((rating: Rating) => (ratingsMean = ratingsMean + rating.attributes.rating));
+  !!ratings && ratings.forEach((rating: RatingType) => (ratingsMean = ratingsMean + rating.attributes.rating));
 
   return {
     mean: !!ratings?.length ? ratingsMean / ratings?.length : 0,
